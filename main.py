64ae3403bf348bb7407b7cc7d3c31c78d50709a4
Mounisha0114 <151064852+Mounisha0114@users.noreply.github.com>
2026-06-11 14:36:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
 from google import genai
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-client = genai.Client(api_key = "")
+
+client = genai.Client(api_key=os.getenv("API_KEY"))
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-app.run(port="5000")
+app.run(port=5000)
